22d7a233872e6ebfee58409d0f3b003fc902c38a
Calvin Lee <calvinlee@Calvins-MacBook-Air.local>
2016-08-20 09:34:43 -0700

    server running

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var riot = require('riot-games-api-nodejs');
+// var riot = require('riot-games-api-nodejs');
 var app = express();
 
 app.use(bodyParser.json());
@@ -8,8 +8,13 @@ app.use(bodyParser.json());
 
 app.set('port', process.env.PORT || 3000);
 
+app.listen(app.get('port'), function() {
+  console.log('Listening on port', app.get('port'))
+})
+
 app.get('/', function(req, res, next){
   res.send("INDEX PAGE");
 });
+
 
 module.exports = app;
